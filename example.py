1c7539a65d8ab43491f29f37b653d74a0fbc8781
Ruth Ogunnaike (Aquent) <v-ruogun@microsoft.com>
2017-10-05 14:26:00 -0700

    Linux support

---
+++ example.py
@@ -0,0 +1,83 @@
+#----------------------------------------------------------------------------------
+# Microsoft Developer & Platform Evangelism
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+#----------------------------------------------------------------------------------
+# The example companies, organizations, products, domain names,
+# e-mail addresses, logos, people, places, and events depicted
+# herein are fictitious.  No association with any real company,
+# organization, product, domain name, email address, logo, person,
+# places, or events is intended or should be inferred.
+#----------------------------------------------------------------------------------
+
+
+import os, uuid, sys
+from azure.storage import CloudStorageAccount
+from azure.storage.blob import BlockBlobService, PublicAccess
+
+
+def run_sample():
+    # Create a CloudStorageAccount instance pointing to your storage account.
+    account_name = 'storageblobdotnetrqs'
+    account_key = 'BjdhmIEqigYQCAZf1vgrpoXuAU3Jna53GlCx+CctdoqnIgYdfFhCm8J4ZMyhWd09yDlSLRH3adw7kPRaZ9Qlaw=='
+
+    try:
+        storage_account = CloudStorageAccount(account_name, account_key)
+
+        # Create the BlockBlockService that is used to call the Blob service for the storage account
+        block_blob_service = storage_account.create_block_blob_service()
+
+        # Create a container called 'quickstartblobs'.
+        container_name ='quickstartblobs'
+        block_blob_service.create_container(container_name)
+
+        # Set the permission so the blobs are public.
+        block_blob_service.set_container_acl(container_name, public_access=PublicAccess.Container)
+
+        # Create a file in Documents to test the upload and download.
+        local_path=os.path.expanduser("~\Documents")
+        local_file_name ="QuickStart_" + str(uuid.uuid4()) + ".txt"
+        full_path_to_file =os.path.join(local_path, local_file_name)
+
+        # Write text to the file.
+        file = open(full_path_to_file,  'w')
+        file.write("Hello, World!")
+        file.close()
+
+        print("Temp file = " + full_path_to_file)
+        print("\nUploading to Blob storage as blob" + local_file_name)
+
+        # Upload the created file, use local_file_name for the blob name
+        block_blob_service.create_blob_from_path(container_name, local_file_name, full_path_to_file)
+
+        # List the blobs in the container
+        print("\nList blobs in the container")
+        generator = block_blob_service.list_blobs(container_name)
+        for blob in generator:
+            print("\t Blob name: " + blob.name)
+
+        # Download the blob(s).
+        # Add '_DOWNLOADED' as prefix to '.txt' so you can see both files in Documents.
+        full_path_to_file2 = os.path.join(local_path, str.replace(local_file_name ,'.txt', '_DOWNLOADED.txt'))
+        print("\nDownloading blob to " + full_path_to_file2)
+        block_blob_service.get_blob_to_path(container_name, local_file_name, full_path_to_file2)
+
+        sys.stdout.write("Sample finished running. When you hit <any key>, the sample will be deleted and the sample "
+                         "application will exit.")
+        sys.stdout.flush()
+        input()
+
+        # Clean up resources. This includes the container and the temp files
+        block_blob_service.delete_blob(container_name, local_file_name)
+        block_blob_service.delete_container(container_name)
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    run_sample()
+
+
+
