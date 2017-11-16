89e7679dea819a0643aa1b356d46b73342064150
Ruth Ogunnaike (Aquent) <v-ruogun@microsoft.com>
2017-11-16 15:27:35 -0800

    Initial commit

--- example.py
+++ example.py
@@ -1,16 +1,27 @@
 # ----------------------------------------------------------------------------------
-# Microsoft Developer & Platform Evangelism
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
-# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES
-# OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+# MIT License
+#
+# Copyright(c) Microsoft Corporation. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 # ----------------------------------------------------------------------------------
-# The example companies, organizations, products, domain names,
-# e-mail addresses, logos, people, places, and events depicted
-# herein are fictitious.  No association with any real company,
-# organization, product, domain name, email address, logo, person,
-# places, or events is intended or should be inferred.
-# ----------------------------------------------------------------------------------
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
 
 import os, uuid, sys
 from azure.storage.blob import BlockBlobService, PublicAccess
